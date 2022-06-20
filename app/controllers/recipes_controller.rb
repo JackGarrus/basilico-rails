8d2288789c55b8e0c4a90df4bb30cb70b1bf10dd
Nadia Guarracino <nad.guar@gmail.com>
2022-06-20 19:46:29 +0200

    added controller + set root route to index + added index view

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,13 @@
+class RecipesController < ApplicationController
+  def index
+    #@recipes = Recipes.all()
+  end
+
+  def new
+  #  @recipes = Recipes.new()
+  end
+
+  def save
+ #   @recipes = Recipes.save()
+  end
+end
